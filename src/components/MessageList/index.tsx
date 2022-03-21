780da5b3c2eabc3b35e110eedf1e83461133abb9
Gui-dev <adrianon10@gmail.com>
2022-03-21 03:39:28 -0300

    feat: fetching data from api

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import styles from './message-list.module.scss'
 import logoImg from './../../assets/images/logo.svg'
+import { api } from './../../services/api'
+
+interface MessagesProps {
+  id: string
+  text: string
+  created_at: string
+  user: {
+    id: string
+    github_id: string
+    name: string
+    login: string
+    avatar_url: string
+  }
+}
 
 export const MessageList: React.FC = () => {
+  const [messages, setMessages] = useState<MessagesProps[]>([])
+
+  useEffect(() => {
+    api.get('/messages/last-messages')
+      .then(response => setMessages(response.data))
+  }, [])
+
   return (
     <section className={styles.messageListWrapper}>
       <img src={logoImg} alt="Do While 2021" title="Do WHile 2021"/>
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certza vai ser o melhor de todos.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/Gui-dev.png" alt="Gui Silva" />
-            </div>
-            <h1>Gui Silva</h1>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certza vai ser o melhor de todos.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/Gui-dev.png" alt="Gui Silva" />
-            </div>
-            <h1>Gui Silva</h1>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certza vai ser o melhor de todos.
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/Gui-dev.png" alt="Gui Silva" />
-            </div>
-            <h1>Gui Silva</h1>
-          </div>
-        </li>
+        { messages && messages.map(message => {
+          return (
+              <li key={ message.id } className={styles.message}>
+                <p className={styles.messageContent}>
+                  { message.text }
+                </p>
+                <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                    <img src={ message.user.avatar_url } alt={ message.user.name } title={ message.user.name }/>
+                  </div>
+                  <h1>{ message.user.name }</h1>
+                </div>
+              </li>
+          )
+        })
+        }
       </ul>
     </section>
   )
