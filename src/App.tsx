7466b774903840b6118b9086be59b49efbc3be54
Gui-dev <adrianon10@gmail.com>
2022-03-22 03:35:52 -0300

    feat: add component AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 import styles from './assets/styles/app.module.scss'
 
+import { AuthProvider } from './contexts/auth'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
 
 function App () {
   return (
-    <main className={styles.contentWrapper}>
-      <MessageList />
-      <LoginBox />
-    </main>
+    <AuthProvider>
+      <main className={styles.contentWrapper}>
+        <MessageList />
+        <LoginBox />
+      </main>
+    </AuthProvider>
   )
 }
 
