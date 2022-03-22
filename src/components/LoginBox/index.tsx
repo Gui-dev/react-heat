c020c864e38afc00263a57abfb7bf197b6dabd6c
Gui-dev <adrianon10@gmail.com>
2022-03-22 03:33:58 -0300

    feat: fetching data from api

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { VscGithubInverted } from 'react-icons/vsc'
 
 import styles from './login.module.scss'
+import { useAuth } from '../../hooks/useAuth'
 
-export const LoginBox: React.FC = () => {
+export const LoginBox = () => {
+  const clientId = 'f8ef90cfffd1c1f9c0bc'
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${clientId}`
+  const { signIn } = useAuth()
+
+  useEffect(() => {
+    const url = window.location.href
+    const hasGithubCode = url.includes('?code=')
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+      window.history.pushState({}, '', urlWithoutCode)
+
+      signIn(githubCode)
+    }
+  }, [signIn])
+
   return (
     <section className={styles.loginBoxWrapper}>
       <h1>Entre e compartilhe sua mensagem</h1>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size={24}/>
         Entrar com Github
       </a>
